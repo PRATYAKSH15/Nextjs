e657e0b190a09a6e890a13032629139656ccb2e3
Pratyaksh <pratyaksh1594@gmail.com>
2025-07-26 19:45:58 +0530

    Navbar added

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <a href="/" className="text-white text-lg font-bold">
+          My App
+        </a>
+        <div>
+          <a href="/about" className="text-gray-300 hover:text-white px-3">
+            About
+          </a>
+          <a href="/contact" className="text-gray-300 hover:text-white px-3">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
