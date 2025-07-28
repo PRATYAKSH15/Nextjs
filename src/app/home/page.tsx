a159af2fb08b4706ae047dfc7e93611e088d545f
Pratyaksh <pratyaksh1594@gmail.com>
2025-07-29 00:14:28 +0530

    added link

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Home() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+    
+
+    <div className='flex flex-col items-center justify-center min-h-screen bg-black-100'>
       <h1>Welcome to My App</h1>
       <p>This is the home page of our application.</p>  
+      <Link href="/about" className="text-blue-500 hover:underline">
+        Go to About Page   
+        </Link>
     </div>
   )
 }
