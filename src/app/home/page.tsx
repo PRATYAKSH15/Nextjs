9e8c2cfb88b380a6f9b6116ca23b53cf732d8f13
Pratyaksh <pratyaksh1594@gmail.com>
2025-07-28 23:59:20 +0530

    added content in app

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      
+      <h1>Welcome to My App</h1>
+      <p>This is the home page of our application.</p>  
     </div>
   )
 }
