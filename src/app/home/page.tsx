441f03a2ab3ee931af00420ef85718cc1b8c26c3
Pratyaksh <pratyaksh1594@gmail.com>
2025-07-29 00:00:01 +0530

    added more

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
       <h1>Welcome to My App</h1>
       <p>This is the home page of our application.</p>  
     </div>
